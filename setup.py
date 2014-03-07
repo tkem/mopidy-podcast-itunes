6498d93a21498ff109a5473704a8a275576bbac8
Thomas Kemmer <tkemmer@computer.org>
2014-03-07 05:47:22 +0100

    Add dependency on Mopidy-Podcast

--- setup.py
+++ setup.py
@@ -25,7 +25,7 @@ setup(
     install_requires=[
         'setuptools',
         'Mopidy >= 0.18',
-        'Pykka >= 1.1',
+        'Mopidy-Podcast >= 0.3.0',
         'requests >= 2.0.0',
     ],
     test_suite='nose.collector',
